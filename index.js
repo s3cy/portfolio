7d3f96414f83cbe5bf575d6033fbca470530b8c2
Chuyang Zheng <s3cy@hotmail.com>
2017-04-09 13:35:58 -0400

    setup for heroku deployment

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var app = express();
 var path = require('path');
 var http = require('http').Server(app);
 
-app.use('/', express.static(__dirname));
+app.set('port', (process.env.PORT) || 8080); // for heroku deployment
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-http.listen(8080, function() {
-    console.log('listening on *:8080');
+http.listen(app.get('port'), function() {
+    console.log(`listening on *:${app.get('port')}`);
 });
